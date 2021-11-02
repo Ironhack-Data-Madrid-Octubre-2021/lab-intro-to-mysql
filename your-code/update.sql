453f7aa44854d7f0cd9295287d57f4e7cb8ef854
JuanVegaPenichet <j.vegapenichet@gmail.com>
2021-11-02 18:36:02 +0100

    Lab terminado

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE Salesperson
+SET store = 'Miami'
+WHERE ID = 4;
+
+UPDATE Customers
+SET Email = 'ppicasso@gmail.com'
+WHERE ID = 0;
+
+UPDATE Customers
+SET Email = 'lincoln@us.gov'
+WHERE ID = 1;
+
+UPDATE Customers
+SET Email = 'hello@napoleon.me'
+WHERE ID = 2;
