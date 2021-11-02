ba536d2749b9fd80864472712dfec18814f4e947
mariaperezdeayalas <mperezdeayalas@gmail.com>
2021-11-02 18:35:05 +0100

    Lab MySQL Solutions hecho

---
+++ your-code/delete.sql
@@ -0,0 +1,3 @@
+DELETE FROM Cars WHERE ID = 4;
+
+
---
+++ your-code/update.sql
@@ -0,0 +1,16 @@
+UPDATE Salespersons
+SET Store = 'Miami'
+WHERE ID = 4;
+
+UPDATE Customers
+SET email = 'ppicasso@gmail.com'
+WHERE ID = 0;
+
+UPDATE Customers
+SET email = 'lincoln@us.gov'
+WHERE ID = 1;
+
+UPDATE Customers
+SET email = 'hello@napoleon.me'
+WHERE ID = 2;
+
