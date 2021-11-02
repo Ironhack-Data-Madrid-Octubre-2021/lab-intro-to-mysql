d2a7403f6eb9e912e7be45b01986ec9d92b032d7
NicolasPce <nicoponcegarcia@gmail.com>
2021-11-02 18:13:03 +0100

    MySQL Nico

---
+++ your-code/update.sql
@@ -0,0 +1,3 @@
+UPDATE Customers SET email = "ppicasso@gmail.com" where Name = 'Pablo Picasso';
+UPDATE Customers SET email = "lincoln@us.gov" where Name = 'Abraham Lincoln';
+UPDATE Customers SET email = "hello@napoleon.me" where Name = 'Napoléon Bonaparte';
