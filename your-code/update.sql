374b0588315ebe35fbf91bfc92bca9301511d54a
TheIronhidex <92517380+TheIronhidex@users.noreply.github.com>
2021-11-02 19:04:56 +0100

    Add files via upload

---
+++ your-code/update.sql
@@ -0,0 +1,16 @@
+UPDATE salespersons
+SET Store = 'Miami'
+WHERE id = 4;
+
+UPDATE customers
+SET Email = 'ppicasso@gmail.com'
+WHERE Name = 'Pablo Picasso';
+
+UPDATE customers
+SET Email = 'lincoln@us.gov'
+WHERE Name = 'Abraham Lincoln';
+
+UPDATE customers
+SET Email = 'hello@napoleon.me'
+WHERE Name = 'Napoléon Bonaparte';
+
