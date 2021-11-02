90a22a7c2f49d6e72d79d3c812aa2bf16b502ac5
daniela-arias <daniarias750@gmail.com>
2021-11-02 18:26:20 +0100

    1ros cambios con error

---
+++ your-code/seeding.sql
@@ -0,0 +1,42 @@
+INSERT INTO cars VALUES
+(0,'3K096I98581DHSNUP','Volkswagen','Tiguan',2019,'azul'),
+(1,'ZM8G7BEUQZ97IH46V','Peugeot','Rifter',2019,'rojo'),
+(2,'RKXVNNIHLVVZOUB4M','Vado','Fusión',2018,'blanco'),
+(3,'HKNDGS7CU31E9Z7JW','Toyota','RAV4',2018,'Plata'),
+(4,'DAM41UDN3CHU2WVF6','Volvo','V60',2019,'gris'),
+(5,'DAM41UDN3CHU2WVF7','Volvo','V60 de campo traviesa',2019,'gris')
+;
+
+INSERT INTO Customers VALUES
+(10001,'Pablo Picasso',34636176382,'pablopicasso@gmail.com','Paseo de la Chopera, 14','Madrid','Madrid','España',28045),
+(20001,'Abraham Lincoln',13059077086,'abrahamlincoln@gmail.com','120 SW 8th St','Florida','Miami','Estados Unidos',33130),
+(30001,'Napoleón Bonaparte',33179754000,'napoleon@gmail.com','40 Rue du Colisée','París','Francia',75008)
+;
+
+INSERT INTO invoices VALUES
+(852399038,22-08-2018,0,1,3),
+(731166526,31-12-2018,3,0,5),
+(271135104,22-01-2019,2,2,7),
+(271135105,23-01-2019,5,4,3),
+(271135106,25-01-2019,7,5,2),
+(271135107,26-01-2019,7,5,2)
+;
+
+INSERT INTO salespersons VALUES
+(00001,'Petey Cruiser','Madrid'),
+(00002,'Anna Sthesia','Barcelona'),
+(00003,'Paul Molive','Berlina'),
+(00004,'Gail Forcewind','París'),
+(00005,'Paige Turner','Mimia'),
+(00006,'Bob Frapples','Ciudad de México'),
+(00007,'Walter Melon','Amsterdam'),
+(00008,'Shonda Leer','Sao Paulo')
+;
+
+
+
+
+
+
+
+
