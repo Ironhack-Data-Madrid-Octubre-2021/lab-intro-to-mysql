bd0dff3c3ce75811131b1b2c056cbc0c7995e9c8
Pnat1 <gperinatcarvajal@gmail.com>
2021-11-03 13:00:39 +0100

    your-code/create.sql

---
+++ your-code/seeding.sql
@@ -0,0 +1,40 @@
+insert into CARS values
+(0, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue');
+insert into CARS values
+(1, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red');
+insert into CARS values
+(2, 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White');
+insert into CARS values
+(3, 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver');
+insert into CARS values
+(4, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray');
+insert into CARS values
+(5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
+insert into CUSTOMERS values
+(10001, 'Pablo Picasso', '+34 636 17 63 82', '-', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045');
+insert into CUSTOMERS values
+(20001, 'Abraham Lincoln', '+1 305 907 7086', '-', '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130');
+insert into CUSTOMERS values
+(30001, 'Napoléon Bonaparte' '+33 1 79 75 40 00', '-', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France' '75008');
+insert into INVOICES values
+(0, '852399038', 22-08-2018, '0', '1', '3');
+insert into INVOICES values
+(1, '731166526', 31-12-2018, '3','0', '5');
+insert into INVOICES values
+(2, '271135104', 22-01-2019, '2', '2', '7');
+insert into SALESPERSONS values
+(0, '00001', 'Petey Cruiser', 'Madrid');
+insert into SALESPERSONS values
+(1, '00002', 'Anna Sthesia', 'Barcelona');
+insert into SALESPERSONS values
+(2, '00003', 'Paul Molive', 'Berlin');
+insert into SALESPERSONS values
+(3, '00004', 'Gail Forcewind', 'Paris');
+insert into SALESPERSONS values
+(4, '00005', 'Paige Turner', 'Mimia');
+insert into SALESPERSONS values
+(5, '00006', 'Bob Frapples' 'Mexico City');
+insert into SALESPERSONS values
+(6, '00007', 'Walter Melon', 'Amsterdam');
+insert into SALESPERSONS values
+(7, '00008', 'Shonda Leer', 'São Paulo');
