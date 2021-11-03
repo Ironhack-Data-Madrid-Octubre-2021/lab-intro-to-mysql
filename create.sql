bb05d67dfbaf92e19576e346c96fc566874aae0c
LuisSerranoCerame <serranocerameluis@gmail.com>
2021-11-03 08:57:18 +0100

    Lab completo

--- create.sql
+++ create.sql
@@ -1,33 +1,95 @@
-INSERT INTO Cars VALUES
-(0,'3K096I98581DHSNUP','Volkswagen','Tiguan',2019,'Blue');
-INSERT INTO Cars VALUES
-(1,'ZM8G7BEUQZ97IH46V','Peugeot','Rifter', 2019,'Red');
-INSERT INTO Cars VALUES
-(2,'RKXVNNIHLVVZOUB4M','Ford','Fusion',2018,'White');
-INSERT INTO Cars VALUES
-(3,'HKNDGS7CU31E9Z7JW','Toyota','RAV4',2018,'Silver');
-INSERT INTO Cars VALUES
-(4,'DAM41UDN3CHU2WVF6','Volvo','V60',2019,'Gray');
-INSERT INTO Cars VALUES
-(5,'DAM41UDN3CHU2WVF','Volvo','V60 Cross Country',2019,'Gray');
+-- MySQL Workbench Forward Engineering
 
-INSERT INTO Customers VALUES
-(0,10001,'Pablo Picasso','+34 636 17 63 82','-','Paseo de la Chopera, 14','Madrid','Madrid','Spain',28045),
-(1,20001,'Abraham Lincoln','+1 305 907 7086','-','120 SW 8th St','Miami','Florida','United States',33130),
-(2,30001,'Napoléon Bonaparte','+33 1 79 75 40 00','-','40 Rue du Colisée','Paris','Île-de-France','France',75008);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-INSERT INTO Salespersons Values
-(0,00001,'Petey Cruiser','Madrid'),
-(1,00002,'Anna Sthesia','Barcelona'),
-(2,00003,'Paul Molive','Berlin'),
-(3,00004,'Gail Forcewind','Paris'),
-(4,00005,'Paige Turner','Mimia'),
-(5,00006,'Bob Frapples','Mexico City'),
-(6,00007,'Walter Melon','Amsterdam'),
-(7,00008,'Shonda Leer','São Paulo');
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 
-INSERT INTO Invoices Values
-(0,'852399038','2018-08-22','0','3','1'),
-(1,'731166526','2018-12-31','3','5','0'),
-(2,'271135104','2019-01-22','2','7','2');
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
+-- -----------------------------------------------------
+-- Table `mydb`.`Cars`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Cars` (
+  `ID` INT NOT NULL,
+  `VIN` VARCHAR(45) NOT NULL,
+  `Manufacturer` VARCHAR(45) NULL,
+  `Model` VARCHAR(45) NULL,
+  `Year` YEAR NULL,
+  `Color` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Customers` (
+  `ID` INT NOT NULL,
+  `Customers ID` VARCHAR(45) NULL,
+  `Name` VARCHAR(45) NULL,
+  `Phone` VARCHAR(45) NULL,
+  `Email` VARCHAR(45) NULL,
+  `Address` VARCHAR(45) NULL,
+  `City` VARCHAR(45) NULL,
+  `State/Province` VARCHAR(45) NULL,
+  `Country` VARCHAR(45) NULL,
+  `Postal` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Salespersons`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Salespersons` (
+  `ID` INT NOT NULL,
+  `Staff ID` VARCHAR(45) NULL,
+  `Name` VARCHAR(45) NULL,
+  `Store` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Invoices`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Invoices` (
+  `ID` INT UNSIGNED NOT NULL,
+  `Invoice Number` VARCHAR(45) NULL,
+  `Date` VARCHAR(45) NULL,
+  `Cars_ID` INT NOT NULL,
+  `Salespersons_ID` INT NOT NULL,
+  `Customers_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_Invoices_Cars_idx` (`Cars_ID` ASC) VISIBLE,
+  INDEX `fk_Invoices_Salespersons1_idx` (`Salespersons_ID` ASC) VISIBLE,
+  INDEX `fk_Invoices_Customers1_idx` (`Customers_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_Invoices_Cars`
+    FOREIGN KEY (`Cars_ID`)
+    REFERENCES `mydb`.`Cars` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Invoices_Salespersons1`
+    FOREIGN KEY (`Salespersons_ID`)
+    REFERENCES `mydb`.`Salespersons` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Invoices_Customers1`
+    FOREIGN KEY (`Customers_ID`)
+    REFERENCES `mydb`.`Customers` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
